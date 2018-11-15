da5baa73b8cc2b7ab9dced89b69388ae62f72ea8
1m188 <228117330@qq.com>
2018-11-15 12:14:49 +0800

    发布版本相关设置

--- Gobang/main.cpp
+++ Gobang/main.cpp
@@ -3,6 +3,10 @@
 
 int main(int argc, char *argv[])
 {
+#ifdef NDEBUG
+	QApplication::addLibraryPath("./plugins"); //发布版本从程序所在目录下寻找相关组件
+#endif // NDEBUG
+
 	QApplication a(argc, argv);
 	Director::getInstance()->setWindow(new Window());
 	Director::getInstance()->getWindow()->show();
