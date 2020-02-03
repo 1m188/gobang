5766fcb4fbfff22ee0b217405a84be8958634c6f
1m188 <228117330@qq.com>
2020-02-03 16:01:30 +0800

    添加cmake构建

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include <QtWidgets/QApplication>
+#include "QApplication"
 #include "Director.h"
 #include "Config.h"
 
 int main(int argc, char *argv[])
 {
-#ifdef NDEBUG
+#ifdef RELEASE
 	QApplication::addLibraryPath("./plugins"); //发布版本从程序所在目录下寻找相关组件
-#endif // NDEBUG
+#endif // RELEASE
 
 	QApplication a(argc, argv);
 	Config::getInstance()->init();
